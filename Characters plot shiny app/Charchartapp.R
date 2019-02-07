3c8d20568f78b395adc12ae5894b22586844a7f4
kdctran <nabbongoug@ucla.edu>
2019-02-06 22:02:12 -0800

    make ggplot make shiny menu

--- Characters plot shiny app/Charchartapp.R
+++ Characters plot shiny app/Charchartapp.R
@@ -2,14 +2,37 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 
+charlist <- read_rds("characterlist.rds")
 
 # user interface
 ui <- fluidPage(
-  titlePanel("Characters' Strength vs. HP"),
+  # title
+  titlePanel("Dragalia Lost - Characters' Strength vs. HP"),
   
+  # sidebar
   sidebarLayout(
     sidebarPanel(
+      selectInput(inputId = "Element", 
+                  label = "Select an Element",
+                  choices = c("All", distinct(charlist, Element)),
+                  selected = NULL,
+                  multiple = F),
       
+      selectInput(inputId = "method", 
+                  selected = NULL,
+                  label = "Select a weapon Type",
+                  choices = c("All", distinct(charlist, Weapon))),
+      
+      selectInput(inputId = "method", 
+                  selected = NULL,
+                  label = "Select a Class",
+                  choices = c("All", distinct(charlist, Class))),
+      width = 2
+    ),
+    
+    # main panel
+    mainPanel(
+      plotOutput(outputId = "charPlot")
     )
   )
 )
@@ -17,7 +40,35 @@ ui <- fluidPage(
 
 # server function
 server <- function(input, output) {
-  
+  output$charPlot <- renderPlot({
+    
+    # filter by element
+    charfilter <- charlist %>%
+      filter(if (input$Element != "All") {
+               Element == input$Element
+             } else TRUE) %>%
+      filter(if (input$Weapon != "All") {
+        Element == input$Weapon
+      } else TRUE) %>%
+      filter(if (input$Class != "All") {
+        Element == input$Class
+      } else TRUE)
+    
+    charfilter %>%
+      ggplot() +
+      geom_point(aes(HP, STR, colour = Element), size = 3) +
+      scale_color_manual(values = c("Flame" = "red2", 
+                                    "Water" = "dodgerblue1",
+                                    "Wind" = "green",
+                                    "Light" = "gold",
+                                    "Shadow" = "purple")) +
+      theme_bw() + 
+      theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  }
+    
+  )
 }
 
+# run the application 
 shinyApp(ui = ui, server = server)
--- Characters plot shiny app/Charhelper.R
+++ Characters plot shiny app/Charhelper.R
@@ -1,9 +1,29 @@
 library(googlesheets)
 library(tidyverse)
 library(ggplot2)
+library(reshape2)
 
 my_sheets <- gs_ls() #get list of google sheet in my account
 be <- gs_title("Characters")
 gs_ws_ls(be)
 char <- gs_read(ss=be, ws = "Sheet1", skip=0)
+
+path <- getwd()
+
+write_rds(char, path = paste0(path, "/Characters plot shiny app/characterlist.rds"))
+
+p <- char %>%
+  ggplot() +
+  geom_point(aes(HP, STR, colour = Element), size = 3) +
+  scale_color_manual(values = c("Flame" = "red2", 
+                                "Water" = "dodgerblue1",
+                                "Wind" = "green",
+                                "Light" = "gold",
+                                "Shadow" = "purple")) +
+  theme_bw() + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+# p + geom_abline(slope = 1, intercept = 0) +
+#   lims(x = c(600,850), y = c(390,750))
 
